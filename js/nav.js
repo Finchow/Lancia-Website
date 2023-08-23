f87293d2732381aaac8c6cd9a924c62d641e3f21
Oscar White <oscar.white361@icloud.com>
2023-08-23 10:47:23 +0100

    First Commit

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+        <div class="navbar">
+            <div class="dropdown">
+            <button class="dropbtn"> Cars <i>▼</i>
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="cars.html#Fulvia">Lancia Fulvia Sport</a>
+                <a href="cars.html#Delta">Lancia Delta</a>
+                <a href="cars.html#Appia">Lancia Appia GTE</a>
+            </div>
+        </div>
+        <a href="index.html">Home</a>
+        <a href="gallery.html">Gallery</a>
+    `;
+}
+
+createNav();
